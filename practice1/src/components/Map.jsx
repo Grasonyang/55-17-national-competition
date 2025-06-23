ea961b38fc0c4d6509d1e5eeaf4ece76d5a40210
Grasonyang <grasonjas@gmail.com>
2025-06-23 13:57:27 +0800

    Map component Update

--- practice1/src/components/Map.jsx
+++ practice1/src/components/Map.jsx
@@ -1,136 +1,97 @@
-import React, { useState } from 'react';
-import './Map.css';
-import attractions1 from '../assets/image/attractions1.jpg';
-import attractions2 from '../assets/image/attractions2.jpg';
-import attractions3 from '../assets/image/attractions3.jpg';
-import map from '../assets/image/map.png';
+import { useState } from "react";
 
 function Map() {
-    let [hoversite, setHoversite] = useState(0);
     let sites = [
         {
             id: 1,
-            name: "景點1",
-            image: attractions1,
-            description: "這是景點1的描述。",
+            name: "景點一",
+            description: "景點一的描述",
+            img: "src/assets/image/1.jpg",
+            alt: "景點一的圖片",
             position: {
-                top: "10%",
-                left: "50%"
+                top: "70%",
+                left: "80%"
             }
-        },
-        {
+        }, {
             id: 2,
-            name: "景點2",
-            image: attractions2,
-            description: "這是景點2的描述。",
+            name: "景點二",
+            description: "景點二的描述",
+            img: "src/assets/image/2.jpg",
+            alt: "景點二的圖片",
             position: {
-                top: "80%",
+                top: "50%",
                 left: "40%"
             }
-        },
-        {
+        }, {
             id: 3,
-            name: "景點3",
-            image: attractions3,
-            description: "這是景點3的描述。",
+            name: "景點三",
+            description: "景點三的描述",
+            img: "src/assets/image/A.png",
+            alt: "景點三的圖片",
             position: {
-                top: "60%",
-                left: "20%"
+                top: "10%",
+                left: "90%"
             }
-        }
-    ]
-    
-    let cardHover = {
-        "transform": "scale(1.05)",
-        "boxShadow": " 0px 0px 18px 4px #0080BC",
-        "background": "linear-gradient(to left, #3f87a6, #ebf8e1, #f69d3c)",
-        // "animation": "1s flowBackground linear infinite"
-    };
-    let linkHover = {
-        // "transform": "scale(1.1)",
-        "textShadow": " 2px 2px 4px  #0080BC"
-    };
-    let mapPointHover = {
-        "transform": "scale(1.05)",
-        "backgroundcolor": "#1000FF",
-        "color": "white",
-        "boxShadow": " 0px 0px 18px 4px #0080BC"
-    }
+        },
 
+    ]
+    let [hoverid, setHoverId] = useState(0);
+    let [cardclass, setCardclass] = useState("card col-5 p-0");
+    let [pointclass, setPointclass] = useState("");
+    let [linkclass, setLinkclass] = useState("");
+    const hoverCard = (id) => {
+        setHoverId(id);
+    }
+    const hoverPoint = () => {
+
+    }
+    const hoverLink = () => {
+
+    }
     return (
         <>
-            <h2 className="fw-bold text-center mt-5" id="map">地圖景點</h2>
-            <div className="container row p-2">
-                <div className="col-12 col-xl-6">
-                    <div className="row gap-3 d-flex justify-content-center">
-                        {
-                            sites.map((site) => {
-                                return (
-                                    <div className="card site-card col-12 col-md-5 site-transition " key={site.id}
-                                        onMouseEnter={() => {
-                                            setHoversite(site.id)
-                                        }}
-                                        onMouseLeave={() => {
-                                            setHoversite(null)
-                                        }}
-                                        style={hoversite === site.id ? cardHover : {}}
-                                    >
-                                        <img src={site.image} className="card-img-top" alt={site.name} />
-                                        <div className="card-body">
-                                            <h5 className="card-title">{site.name}</h5>
-                                            <p className="card-text">{site.description}</p>
-                                            <a href="#" className="btn btn-primary">了解更多</a>
+            <div className="map_container mt-5">
+                <h2>地圖景點</h2>
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="row gap-3">
+                            {
+                                sites.map((site) => {
+                                    return (
+                                        <div className={cardclass} key={site.id}
+                                            onMouseDown={
+                                                () => {
+                                                    hoverCard(site.id);
+                                                }
+                                            }
+                                        >
+                                            <img src={site.img} alt={site.alt} className="card-img-top" />
+                                            <div className="card-body">
+                                                <h4 className="card-title">{site.name}</h4>
+                                                <p className="card-text">{site.description}</p>
+                                            </div>
                                         </div>
-                                    </div>
-                                )
-                            })
-                        }
-                        <div className="card col-12 col-md-5 d-flex flex-column justify-content-center">
-                            <h4>地圖景點</h4>
-                            <ul>
+                                    )
+                                })
+                            }
+                            <div className="link_container card col-5 d-flex flex-column justify-content-center">
+                                <h3>景點連結</h3>
                                 {
                                     sites.map((site) => {
                                         return (
-                                            <li className="nav-link site-transition" key={site.id}
-                                                onMouseEnter={() => {
-                                                    setHoversite(site.id)
-                                                }}
-                                                onMouseLeave={() => {
-                                                    setHoversite(null)
-                                                }}
-                                                style={hoversite === site.id ? linkHover : {}}
-                                            ><a href="#">{site.name}</a></li>
+                                            <a href="" className="link-primary" key={site.id}>
+                                                {site.name}
+                                            </a>
                                         )
                                     })
                                 }
-                            </ul>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="col-12 col-xl-6 d-flex justify-content-center">
-                    <div className='map-container d-flex justify-content-center p-5 p-xl-0'>
-                        <img src={map} className="map" alt="map image" />
-                        {
-                            sites.map((site) => {
-                                return (
-                                    <div className="map-point site-transition" style={hoversite === site.id ? { ...site.position, ...mapPointHover } : site.position} key={site.id}
-                                        onMouseEnter={() => {
-                                            setHoversite(site.id)
-                                        }}
-                                        onMouseLeave={() => {
-                                            setHoversite(null)
-                                        }}
-                                    >
-                                        <span className="point-name">{site.name}</span>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
+                    <div className="col-12 col-md-6">right</div>
                 </div>
             </div>
         </>
     )
 }
-
 export default Map;
