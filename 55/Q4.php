f21252eec18ba74f9cf4264e45698e6a61b69db2
Grasonyang <grasonjas@gmail.com>
2025-06-23 14:41:23 +0800

    finish Q4

--- 55/Q4.php
+++ 55/Q4.php
@@ -1,13 +1,26 @@
 <?php
-$n = trim(fgets(STDIN));
-$ans = [];
-$ans[] = array_fill(0, $n, 1);
-while(1){
-    if(end($ans)===[$n])
-        break;
-    $this_arr = [];
-    
-}
-for($i=0;$i<$n;$i++){
+// leetcode 39
 
+$n = trim(fgets(STDIN));
+$result = [];
+$currentPartition = [];
+
+function backtrack($partition_number,$max_use_partition_number,&$currentPartition,&$result){
+    if ($partition_number === 0) {
+        $result[] = $currentPartition;
+        return;
+    }
+    if ($partition_number < 0) {
+        return;
+    }
+    for($i=min($partition_number,$max_use_partition_number);$i>=1;$i--){
+        $currentPartition[] = $i;
+        backtrack($partition_number - $i, $i, $currentPartition, $result);
+        array_pop($currentPartition);
+    }
+}
+backtrack($n,$n-1,$currentPartition,$result);
+echo count($result)."\n";
+foreach ($result as $partition) {
+    echo implode(" ", $partition) . "\n";
 }
