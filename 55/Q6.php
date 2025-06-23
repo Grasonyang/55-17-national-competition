2b0058a42dec66f61c33e28d06905798fcde4f93
Grasonyang <grasonjas@gmail.com>
2025-06-23 10:12:27 +0800

    finish Q6

--- 55/Q6.php
+++ 55/Q6.php
@@ -1,3 +1,19 @@
 <?php
 $stack = [];
 $input = trim(fgets(STDIN));
+// leetcode 32
+// (()(()
+$max = 0;
+for($i=0;$i<strlen($input);$i++){
+    if($input[$i] == "("){
+        $stack[] = $i;
+    }else{
+        if(count($stack)===0){
+            $stack[] = $i;
+        }else{
+            array_pop($stack);
+            $max = max($max, $i-end($stack));
+        }
+    }
+}
+echo $max . "\n";
