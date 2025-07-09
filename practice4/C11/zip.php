ef961a386add05f08de5559a2ad3faa853b5ca73
Grasonyang <grasonjas@gmail.com>
2025-07-09 18:08:13 +0800

    finish:practice4

--- practice4/C11/zip.php
+++ practice4/C11/zip.php
@@ -12,34 +12,20 @@ $h=imagesy($image);
 for($i=0;$i<$w;$i+=$size){
     for($j=0;$j<$h;$j+=$size){
         $count = $r = $b =$g =0;
-        for($x=0;$x<$size;$x+=$size){
-            for($y=0;$y<$size;$y+=$size){
-                $rgb = imagecolorsforindex($image, imagecolorat($image, $x, $y));
+        for($x=0;$x<$size && $i+$x<$w;$x++){
+            for($y=0;$y<$size && $j+$y<$h;$y++){
+                $rgb = imagecolorsforindex($image, imagecolorat($image, $i+$x, $j+$y));
                 $r+=$rgb['red'];
                 $g+=$rgb['green'];
                 $b+=$rgb['blue'];
+                $count++;
             }
         }
         $r = intdiv($r,$count);
         $g = intdiv($g,$count);
         $b = intdiv($b,$count);
         $color = imagecolorallocate($image,$r,$g,$b);
-        imagecolor
+        imagefilledrectangle($image,$i,$j,$i+$size,$j+$size,$color);
     }
 }
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-</head>
-
-<body>
-    <img src="zip.png" alt="unknown"/>
-</body>
-
-</html>
+imagepng($image);
